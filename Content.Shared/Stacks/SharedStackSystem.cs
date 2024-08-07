18686c07bff897d10f5f04903ec299ba3940a246
dffdff2423 <dffdff2423@gmail.com>
2024-08-07 12:29:38 -0500

    Disable merging stacks with labels (#349)

--- Content.Shared/Stacks/SharedStackSystem.cs
+++ Content.Shared/Stacks/SharedStackSystem.cs
@@ -12,6 +12,9 @@ using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
 
+// CD: imports
+using Content.Shared.Labels.Components;
+
 namespace Content.Shared.Stacks
 {
     [UsedImplicitly]
@@ -114,6 +117,10 @@ namespace Content.Shared.Stacks
                 return false;
 
             if (string.IsNullOrEmpty(recipientStack.StackTypeId) || !recipientStack.StackTypeId.Equals(donorStack.StackTypeId))
+                return false;
+
+            // CD: Cannot merge stacks with labels
+            if (HasComp<LabelComponent>(donor) || HasComp<LabelComponent>(recipient))
                 return false;
 
             transferred = Math.Min(donorStack.Count, GetAvailableSpace(recipientStack));
@@ -327,6 +334,10 @@ namespace Content.Shared.Stacks
         public bool TryAdd(EntityUid insertEnt, EntityUid targetEnt, int count, StackComponent? insertStack = null, StackComponent? targetStack = null)
         {
             if (!Resolve(insertEnt, ref insertStack) || !Resolve(targetEnt, ref targetStack))
+                return false;
+
+            // CD: Don't merge labeled stacks
+            if (HasComp<LabelComponent>(insertEnt) || HasComp<LabelComponent>(targetEnt))
                 return false;
 
             var available = GetAvailableSpace(targetStack);
