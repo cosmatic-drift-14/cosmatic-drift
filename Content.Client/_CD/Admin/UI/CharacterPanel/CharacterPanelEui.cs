4c115c727a53c5f1c128078d17f63046f2cb33bd
lunarcomets <luanrcomets2@gmail,com>
2024-11-18 13:19:57 -0800

    NAME ON WINDOW WOO

--- Content.Client/_CD/Admin/UI/CharacterPanel/CharacterPanelEui.cs
+++ Content.Client/_CD/Admin/UI/CharacterPanel/CharacterPanelEui.cs
@@ -1,14 +1,19 @@
-﻿using Content.Client.Eui;
+﻿using Content.Client.Administration.Managers;
+using Content.Client.Eui;
+using Content.Shared._CD.Admin;
+using Content.Shared.Eui;
 
 namespace Content.Client._CD.Admin.UI.CharacterPanel;
 
 public sealed class CharacterPanelEui : BaseEui
 {
+    [Dependency] private readonly IClientAdminManager _admin = default!;
+
     private CharacterPanel CharacterPanel { get; }
 
     public CharacterPanelEui()
     {
-        CharacterPanel = new CharacterPanel();
+        CharacterPanel = new CharacterPanel(_admin);
     }
 
     public override void Opened()
@@ -19,5 +24,14 @@ public sealed class CharacterPanelEui : BaseEui
     public override void Closed()
     {
         CharacterPanel.Close();
+    }
+
+    public override void HandleState(EuiStateBase state)
+    {
+        if (state is not CharacterPanelEuiState s)
+            return;
+
+        CharacterPanel.TargetPlayer = s.Player;
+        CharacterPanel.SetPlayer(s.Player);
     }
 }
--- Content.Server/_CD/Admin/CharacterPanelEui.cs
+++ Content.Server/_CD/Admin/CharacterPanelEui.cs
@@ -1,12 +1,22 @@
-﻿using Content.Server.EUI;
+﻿using Content.Server.Administration;
+using Content.Server.EUI;
+using Content.Shared._CD.Admin;
+using Content.Shared.Eui;
 
 namespace Content.Server._CD.Admin;
 
 public sealed class CharacterPanelEui : BaseEui
-
 {
-    public CharacterPanelEui()
+    private readonly LocatedPlayerData _targetPlayer;
+
+    public CharacterPanelEui(LocatedPlayerData player)
     {
         IoCManager.InjectDependencies(this);
+        _targetPlayer = player;
+    }
+
+    public override EuiStateBase GetNewState()
+    {
+        return new CharacterPanelEuiState(_targetPlayer.Username);
     }
 }
