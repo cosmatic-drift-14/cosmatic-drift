99e3e63094dcd3837db9c6729c66ce335751feb9
lunarcomets <luanrcomets2@gmail,com>
2024-11-20 21:12:53 -0800

    initial commit

---
+++ Content.Client/_CD/Admin/UI/EventPreferenceEui.cs
@@ -0,0 +1,17 @@
+﻿using Content.Client.Administration.Managers;
+using Content.Client.Eui;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
+
+namespace Content.Client._CD.Admin.UI;
+
+public sealed class EventPreferenceEui : BaseEui
+{
+    [Dependency] private readonly IClientAdminManager _admin = default!;
+
+    private EventPreferences EventPreferencesPanel { get; }
+
+    public EventPreferenceEui()
+    {
+        EventPreferencesPanel = new EventPreferences(_admin);
+    }
+}
---
+++ Content.Server/_CD/Admin/EventPreferenceEui.cs
@@ -0,0 +1,23 @@
+﻿using Content.Server.Administration;
+using Content.Server.EUI;
+using Content.Shared.Preferences;
+
+namespace Content.Server._CD.Admin;
+
+public sealed class EventPreferenceEui : BaseEui
+{
+    private readonly HumanoidCharacterProfile _playerPref;
+    private readonly LocatedPlayerData _targetPlayer;
+
+    public EventPreferenceEui(LocatedPlayerData player, HumanoidCharacterProfile pref)
+    {
+        IoCManager.InjectDependencies(this);
+        _targetPlayer = player;
+        _playerPref = pref;
+    }
+
+    public async void SetState()
+    {
+        StateDirty();
+    }
+}
