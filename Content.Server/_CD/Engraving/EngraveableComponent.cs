a0bdab4689ceaf6cc861f7ab66ef27c747861e09
Milon <plmilonpl@gmail.com>
2024-11-24 05:25:06 +0100

    i'm not a miracle worker, but here's your fix

--- Content.Server/_CD/Engraving/EngraveableComponent.cs
+++ Content.Server/_CD/Engraving/EngraveableComponent.cs
@@ -1,39 +1,32 @@
-using Robust.Shared.GameStates;
-using Robust.Shared.Utility;
-
 namespace Content.Server._CD.Engraving;
 
 /// <summary>
-/// Allows an items' description to be modified with an engraving
+///     Allows an items' description to be modified with an engraving
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, Access(typeof(EngraveableSystem))]
 public sealed partial class EngraveableComponent : Component
 {
     /// <summary>
-    /// Message given to user to notify them a message was sent
+    ///     Message given to user to notify them a message was sent
     /// </summary>
     [DataField]
-    [ViewVariables(VVAccess.ReadWrite)]
     public string EngravedMessage = string.Empty;
 
     /// <summary>
-    /// The inspect text to use when there is no engraving
+    ///     The inspect text to use when there is no engraving
     /// </summary>
     [DataField]
-    [ViewVariables(VVAccess.ReadWrite)]
-    public string NoEngravingText = "engraving-dogtags-no-message";
+    public LocId NoEngravingText = "engraving-dogtags-no-message";
 
     /// <summary>
-    /// The message to use when successfully engraving the item
+    ///     The message to use when successfully engraving the item
     /// </summary>
     [DataField]
-    [ViewVariables(VVAccess.ReadWrite)]
-    public string EngraveSuccessMessage = "engraving-dogtags-succeed";
+    public LocId EngraveSuccessMessage = "engraving-dogtags-succeed";
 
     /// <summary>
-    /// The inspect text to use when there is an engraving. The message will be shown seperately afterwards.
+    ///     The inspect text to use when there is an engraving. The message will be shown seperately afterwards.
     /// </summary>
     [DataField]
-    [ViewVariables(VVAccess.ReadWrite)]
-    public string HasEngravingText = "engraving-dogtags-has-message";
+    public LocId HasEngravingText = "engraving-dogtags-has-message";
 }
