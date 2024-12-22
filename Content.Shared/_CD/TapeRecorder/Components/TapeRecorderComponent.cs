cb0cc5cbf935032db9bcd313991850944b10a69a
PursuitInAshes <pursuitinashes@gmail.com>
2024-12-22 11:25:10 -0700

    Fix YAML Fail/Fix Namespace Issue

--- Content.Shared/_CD/TapeRecorder/Components/TapeRecorderComponent.cs
+++ Content.Shared/_CD/TapeRecorder/Components/TapeRecorderComponent.cs
@@ -58,7 +58,7 @@ public sealed partial class TapeRecorderComponent : Component
     /// What sound is used when play mode is activated
     /// </summary>
     [DataField]
-    public SoundSpecifier PlaySound = new SoundPathSpecifier("/Audio/Items/Taperecorder/taperecorder_play.ogg")
+    public SoundSpecifier PlaySound = new SoundPathSpecifier("/Audio/_CD/Items/Taperecorder/taperecorder_play.ogg")
     {
         Params = AudioParams.Default.WithVolume(-2f).WithMaxDistance(3f)
     };
@@ -67,7 +67,7 @@ public sealed partial class TapeRecorderComponent : Component
     /// What sound is used when stop mode is activated
     /// </summary>
     [DataField]
-    public SoundSpecifier StopSound = new SoundPathSpecifier("/Audio/Items/Taperecorder/taperecorder_stop.ogg")
+    public SoundSpecifier StopSound = new SoundPathSpecifier("/Audio/_CD/Items/Taperecorder/taperecorder_stop.ogg")
     {
         Params = AudioParams.Default.WithVolume(-2f).WithMaxDistance(3f)
     };
@@ -76,7 +76,7 @@ public sealed partial class TapeRecorderComponent : Component
     /// What sound is used when rewind mode is activated
     /// </summary>
     [DataField]
-    public SoundSpecifier RewindSound = new SoundPathSpecifier("/Audio/Items/Taperecorder/taperecorder_rewind.ogg")
+    public SoundSpecifier RewindSound = new SoundPathSpecifier("/Audio/_CD/Items/Taperecorder/taperecorder_rewind.ogg")
     {
         Params = AudioParams.Default.WithVolume(-2f).WithMaxDistance(3f)
     };
