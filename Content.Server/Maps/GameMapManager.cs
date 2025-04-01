fe8137e8c43df83a8e89094895d34a034c00e363
Aexxie <codyfox.077@gmail.com>
2025-04-01 18:15:27 -0500

    Update GameMapManager.cs

--- Content.Server/Maps/GameMapManager.cs
+++ Content.Server/Maps/GameMapManager.cs
@@ -197,9 +197,9 @@ public sealed class GameMapManager : IGameMapManager
 
     private bool IsMapEligible(GameMapPrototype map)
     {
-        return //map.MaxPlayers >= _playerManager.PlayerCount &&
-               //map.MinPlayers <= _playerManager.PlayerCount &&
-               //map.Conditions.All(x => x.Check(map)) &&
+        return map.MaxPlayers >= _playerManager.PlayerCount &&
+               map.MinPlayers <= _playerManager.PlayerCount &&
+               map.Conditions.All(x => x.Check(map)) &&
                _entityManager.System<GameTicker>().IsMapEligible(map);
     }
 
