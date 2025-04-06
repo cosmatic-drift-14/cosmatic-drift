0872a3eada3d2e4c7eba3f7feec72246bf69cb52
lunarcomets <lunarcomets2@gmail.com>
2025-04-06 00:18:12 -0700

    subtype spritepaths

--- Content.Shared/_CD/Silicons/Borgs/BorgSubtypePrototype.cs
+++ Content.Shared/_CD/Silicons/Borgs/BorgSubtypePrototype.cs
@@ -1,6 +1,7 @@
 using Content.Shared.Silicons.Borgs;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
+using Robust.Shared.Utility;
 
 namespace Content.Shared._CD.Silicons.Borgs;
 /// <summary>
@@ -19,8 +20,15 @@ public sealed partial class BorgSubtypePrototype : IPrototype
     public required EntProtoId DummyPrototype;
 
     /// <summary>
+    /// The sprite path belonging to this particular subtype.
+    /// </summary>
+    [DataField]
+    public required ResPath SpritePath;
+
+    /// <summary>
     /// The parent borg type that the subtype will be shown under in the selection menu.
     /// </summary>
     [DataField]
     public required ProtoId<BorgTypePrototype> ParentBorgType = "generic";
+
 }
