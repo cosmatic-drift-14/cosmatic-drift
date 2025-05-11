aa7b64af7845a4a30a28010c5bef60a5802c614c
Ecramox <65426878+Ecramox@users.noreply.github.com>
2025-05-11 13:16:25 -0700

    Unshaded Markings (#625)

--- Content.Shared/Humanoid/Markings/MarkingPrototype.cs
+++ Content.Shared/Humanoid/Markings/MarkingPrototype.cs
@@ -42,6 +42,10 @@ namespace Content.Shared.Humanoid.Markings
         [DataField("sprites", required: true)]
         public List<SpriteSpecifier> Sprites { get; private set; } = default!;
 
+        /// CD change, originally created by Beck for Impstation
+	    [DataField("shader")]
+		public string? Shader { get; private set; } = null;
+
         public Marking AsMarking()
         {
             return new Marking(ID, Sprites.Count);
