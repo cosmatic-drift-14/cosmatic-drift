8f5091f20c698849801371e82c53a584b3a24fca
ash lea <example@thisismyactual.email>
2025-06-14 09:22:36 -0400

    add allergies

---
+++ Content.Server/_CD/Body/Components/AllergyComponent.cs
@@ -0,0 +1,27 @@
+using Content.Server._CD.Body.Systems;
+using Content.Shared.FixedPoint;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Server._CD.Body.Components;
+
+/// <summary>
+/// Causes an allergic reaction when exposed to a reagent.
+/// </summary>
+[RegisterComponent, Access(typeof(AllergySystem))]
+public sealed partial class AllergyComponent : Component
+{
+    /// <summary>
+    /// The next time that reagents will be metabolized.
+    /// </summary>
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
+    public TimeSpan NextUpdate;
+
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public Dictionary<string, FixedPoint2> Reagents = new();
+
+    /// <summary>
+    /// How often to metabolize reagents.
+    /// </summary>
+    [DataField]
+    public TimeSpan UpdateInterval = TimeSpan.FromSeconds(1);
+}
