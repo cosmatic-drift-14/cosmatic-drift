edb439d088b2047f8964ccab64acf95009dafff2
lunarcomets <lunarcomets2@gmail.com>
2025-07-11 00:59:42 -0700

    init commit; take assets from pr #590 and set them into a 'boilerplate' state for new system implementation

---
+++ Content.Server/_CD/Silicons/SwitchableBorgSubstypeSystem.cs
@@ -0,0 +1,11 @@
+using Content.Shared._CD.Silicons;
+
+namespace Content.Server._CD.Silicons;
+
+/// <summary>
+/// Primarily handles the appearance aspects of the borg subtype.
+/// </summary>
+public sealed class SwitchableBorgSubstypeSystem : SharedBorgSwitchableSubtypeSystem
+{
+
+}
---
+++ Content.Shared/_CD/Silicons/BorgSubtypePrototype.cs
@@ -0,0 +1,12 @@
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._CD.Silicons;
+
+/// <summary>
+/// Information relating to a borg's subtype. Should be mostly cosmetic.
+/// </summary>
+public sealed class BorgSubtypePrototype : IPrototype
+{
+    [IdDataField]
+    public required string ID { get; set; }
+}
