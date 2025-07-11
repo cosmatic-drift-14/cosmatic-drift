80b7509a872ea5c106998be1d2d8a26141ffdc4b
lunarcomets <lunarcomets2@gmail.com>
2025-07-11 00:59:42 -0700

    init commit; take assets from pr #590 and set them into a 'boilerplate' state for new system implementation

---
+++ Content.Shared/_CD/Silicons/SharedSwitchableSubstypeSystem.cs
@@ -0,0 +1,14 @@
+namespace Content.Shared._CD.Silicons;
+
+/// <summary>
+/// Shared behaviour for borg switchable subtype logic.
+///
+/// </summary>
+public abstract class SharedBorgSwitchableSubtypeSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+
+    }
+}
---
+++ Content.Shared/_CD/Silicons/SwitchableBorgSubstypeSystem.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared._CD.Silicons;
+
+/// <summary>
+/// Server-side logic for the borg subtype system.
+/// Used for applying subtype features that should not be handled by the client (i.e. sounds)
+/// </summary>
+public sealed class SwitchableBorgSubstypeSystem : SharedBorgSwitchableSubtypeSystem
+{
+
+}
