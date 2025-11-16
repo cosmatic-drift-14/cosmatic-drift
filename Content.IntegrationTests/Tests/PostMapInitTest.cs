43ddb169c2ebe5ed7bcc6e2f7bfca8795ba6dd53
Dogbone10 <falcolom1@outlook.com>
2025-11-16 11:30:52 -0500

    Added a comma -> , <- one of those, very cool.

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -111,7 +111,7 @@ namespace Content.IntegrationTests.Tests
             "Ferrous", // CD Map
             "Aspid", // CD Map
             "Gemini", // CD Map
-            "Cocoon" // CD Map
+            "Cocoon", // CD Map
             "CDCentComm" // CD Map
         };
 
