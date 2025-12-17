e0a2a32a0210ac89825f737eee3f28014672fb32
Nairodian Naxelod <Nairodian@gmail.com>
2025-12-17 12:08:50 -0600

    Test And Pool

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -111,6 +111,7 @@ namespace Content.IntegrationTests.Tests
             "Ferrous", // CD Map
             "Aspid", // CD Map
             "Gemini", // CD Map
+            "Omega", // CD Map
             "CDCentComm" // CD Map
         };
 
