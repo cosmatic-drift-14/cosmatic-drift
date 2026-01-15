52172a0eeac2b3b5680feaf599dda18b65091e16
lunarcomets <lunarcomets2@gmail.com>
2026-01-14 23:09:01 -0800

    visible loadout

---
+++ Content.Shared/_CD/Preferences/Loadouts/LoadoutPrototype.cs
@@ -0,0 +1,9 @@
+using Robust.Shared.Prototypes;
+
+// ReSharper disable once CheckNamespace
+namespace Content.Shared.Preferences.Loadouts;
+
+public sealed partial class LoadoutPrototype
+{
+    public EntProtoId Brain { get; set; } = new();
+}
