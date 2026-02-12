f4d3418cea1dcd72221d74372e1b2c36c97c2fb8
lunarcomets <lunarcomets2@gmail.com>
2026-02-11 22:49:11 -0800

    touch-ups

--- Content.Shared/_CD/ExtendedEquipmentComponent.cs
+++ Content.Shared/_CD/ExtendedEquipmentComponent.cs
@@ -3,12 +3,16 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._CD;
 
 /// <summary>
-/// This is used for...
+/// Component signifying that this entity is 'extending' from an entity
+/// with the <see cref="ExtendableClothingComponent"/> component.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-
+[Access(typeof(ExtendableClothingSystem))]
 public sealed partial class ExtendedEquipmentComponent : Component
 {
+    /// <summary>
+    /// Inventory slot we'll want this equipment to be equipped to.
+    /// </summary>
     [DataField, AutoNetworkedField]
     public string Slot = "default";
 
